35f9d1da4fbb0515baba77667dc7535d4cda75d4
Tharun Raj <THARUNRAJ2023@GMAIL.COM>
2024-11-22 11:33:15 +0530

    problem no 1071

---
+++ src/main/java/org/example/FlipColumns1072.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class FlipColumns1072 {
+        public int maxEqualRowsAfterFlips(int[][] m) {
+            HashMap<String,Integer> map=new HashMap<>();
+            for(int[] a: m){
+                StringBuilder str=new StringBuilder();
+                for(int val:a){
+                    if(a[0]==0){
+                        str.append(val);
+                    }else{
+                        str.append(val == 0?"1":"0");
+                    }
+                }
+                map.put(str.toString(),map.getOrDefault(str.toString(),0)+1);
+            }
+            int ans=0;
+            for(int val:map.values()){
+                ans=Math.max(ans,val);
+            }
+            return ans;
+        }
+}
