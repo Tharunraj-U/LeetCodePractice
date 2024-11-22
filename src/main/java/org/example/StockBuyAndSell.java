94f99d680c7741e2d9618e694ca8eb6629732dda
Tharun Raj <THARUNRAJ2023@GMAIL.COM>
2024-11-22 09:48:38 +0530

    Stock problem in DP

---
+++ src/main/java/org/example/StockBuyAndSell.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class StockBuyAndSell {
+    public int maximumProfit(int ...p) {
+        int P=0,M=Integer.MAX_VALUE;
+        for(int a:p){
+            M=Math.min(a,M);
+            P=Math.max(P,a-M);
+        }
+        return P;
+    }
+    public static void main(String[] args) {
+        System.out.println(new StockBuyAndSell().maximumProfit(765432,5754,4,554637457,643,7676,76,767));
+    }
+}
