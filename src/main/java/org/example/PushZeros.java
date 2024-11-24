724f3f302489d2ced35ef19768bced0c3b866521
Tharun Raj <THARUNRAJ2023@GMAIL.COM>
2024-11-24 12:02:09 +0530

    Rotate an array

--- src/main/java/org/example/PushZeros.java
+++ src/main/java/org/example/PushZeros.java
@@ -1,4 +1,40 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class PushZeros {
+//    optimal solution
+//    void pushZerosToEnd(int[] arr) {
+//        int n=arr.length;
+//        int zero=0;
+//        for(int i=0;i<n;i++){
+//            if(arr[i]==0){
+//                continue;
+//            }
+//            if(zero != i){
+//                arr[zero]=arr[i];
+//                arr[i]=0;
+//            }
+//            zero++;
+//
+//        }
+//    }
+       static void pushZerosToEnd(int[] arr) {
+            int n=arr.length;
+            for(int i=0;i<n;i++){
+                int temp=i;
+                for(int j=i+1;j<n;j++){
+                    if(arr[temp]==0 && arr[j] !=0){
+                        arr[temp]=arr[j];
+                        arr[j]=0;
+                        temp=j;
+                    }
+                }
+            }
+        }
+    public static void main(String[] args) {
+       int a[]={3,5,0,0,4};
+       pushZerosToEnd(a);
+       System.out.print(Arrays.toString(a));
+    }
 }
---
+++ src/main/java/org/example/RotateAnArray.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Arrays;
+public class RotateAnArray {
+    private static void roatate(int[] a, int n) {
+        n=n%n;
+        for(int i=0;i<n;i++){
+            int val=0;
+            for(int j=1;j<a.length;j++){
+                int temp=a[val];
+                a[val]=a[j];
+                a[j]=temp;
+                val=j;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int a[]={7, 3, 9, 1};
+        roatate(a,9);
+        System.out.print(Arrays.toString(a));
+    }
+
+
+}
