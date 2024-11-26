c0848c9298588a2887c74a478fb139f065203103
Tharun Raj <THARUNRAJ2023@GMAIL.COM>
2024-11-26 14:37:31 +0530

    Find Champion I

--- src/main/java/org/example/FindChampionI.java
+++ src/main/java/org/example/FindChampionI.java
@@ -1,4 +1,26 @@
 package org.example;
 
 public class FindChampionI {
+    public int findChampion(int[][] grid) {
+        int i=0,max=0,ans=0;
+        for(int a[]: grid){
+            int c=0;
+            for(int val:a){
+                if(val==1){
+                    c++;
+                }
+            }
+
+            if(max < c){
+                max=c;
+                ans=i;
+                System.out.println(i+" "+c);
+            }
+            i++;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+
+    }
 }
