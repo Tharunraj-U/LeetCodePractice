945aeed45947301463dae380fd88999ce6d41eb5
Tharun Raj <THARUNRAJ2023@GMAIL.COM>
2024-11-28 10:17:04 +0530

    Implement Atoi ' '

--- src/main/java/org/example/ImplementAtoi.java
+++ src/main/java/org/example/ImplementAtoi.java
@@ -3,27 +3,26 @@ package org.example;
 public class ImplementAtoi {
     class Solution {
         public int myAtoi(String s) {
+            s=s.trim();
+            if(s.isEmpty() || s== null){
+                return 0;
+            }
             long ans=0;
             boolean neg=false;
-            s=s.trim();
-            if(s.charAt(0) == '-'){
-                neg=true;
+            int index=0,n=s.length();
+            if(index < n && s.charAt(index) == '-' && s.charAt(index)== '+'){
+                neg=s.charAt(index)=='-';
+                index++;
             }
-            for(char a:s.toCharArray()){
-                if(a == '-'){
-                    continue;
-                }
-                if( a+0 > 57  ){
-                    break;
-                }
-                else{
-                    ans=ans*10+(a-48);
-                }
+            char c;
+            while(index < n && Character.isDigit(c=s.charAt(index))){
+                ans=ans*10+(c-48);
+                index++;
+                if( neg && -ans < Integer.MIN_VALUE)return Integer.MIN_VALUE;
+                if( !neg && ans > Integer.MAX_VALUE)return Integer.MAX_VALUE;
             }
-            if(neg)ans*=-1;
-            if(ans > Integer.MAX_VALUE)return Integer.MAX_VALUE;
-            if(ans < Integer.MIN_VALUE)return Integer.MIN_VALUE;
-            return  (int)ans;
+            ans= neg ? -ans:ans;
+            return (int)ans;
         }
     }
 }
