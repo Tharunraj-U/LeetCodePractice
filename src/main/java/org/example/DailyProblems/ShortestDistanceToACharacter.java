7312280b5e8f6edbdc23cac122e4774e6060f892
Tharun Raj <THARUNRAJ2023@GMAIL.COM>
2024-12-01 17:25:43 +0530

    Shortest Distance to a Character

--- src/main/java/org/example/DailyProblems/ShortestDistanceToACharacter.java
+++ src/main/java/org/example/DailyProblems/ShortestDistanceToACharacter.java
@@ -1,5 +1,48 @@
 package org.example.DailyProblems;
 
-public class ShortestDistanceToACharacter {
+import java.util.ArrayList;
 
+public class ShortestDistanceToACharacter {
+    public int[] shortestToChar(String s, char c) {
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == c){
+                list.add(i);
+            }
+        }
+        int ans[]=new int[s.length()];
+        for(int i=0;i<s.length();i++){
+            char c1=s.charAt(i);
+            if(c1== c){
+                ans[i]=0;
+            }else{
+                int nearIndex=bin(list,i);
+                System.out.print(nearIndex+" "+i);
+                ans[i]=Math.abs(i-nearIndex);
+            }
+
+        }
+        return ans;
+    }
+    public int bin(List<Integer> list,int val){
+        int l=0;
+        int r=list.size()-1;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(list.get(mid) == val){
+                return list.get(mid);
+            }
+            if(list.get(mid) < val){
+                l=mid+1;
+            }
+            else{
+                r=mid;
+            }
+        }
+        int nearest = list.get(l);
+        if (l > 0) { // Check if there's a closer left neighbor
+            nearest = Math.abs(list.get(l - 1) - val) < Math.abs(list.get(l) - val) ? list.get(l - 1) : list.get(l);
+        }
+        return nearest;
+    }
 }
