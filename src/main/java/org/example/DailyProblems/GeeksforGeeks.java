4fd969d79bfd418d7978c58b398d74fd7b2474e5
Tharun Raj <THARUNRAJ2023@GMAIL.COM>
2025-01-09 21:32:59 +0530

    final

---
+++ src/main/java/org/example/DailyProblems/GeeksforGeeks.java
@@ -0,0 +1,29 @@
+package org.example.DailyProblems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class GeeksforGeeks {
+        static ArrayList<Integer> subarraySum(int[] arr, int target) {
+            // Arrays.sort(arr);
+            int sum=0,i=0,j=0;
+            while(i<arr.length){
+
+                if(sum < target){
+                    sum+=arr[i++];
+                }else if(sum > target){
+                    sum-=arr[j++];
+                } else return new ArrayList<>(Arrays.asList(j+1,i));
+                if(sum == target) return new ArrayList<>(Arrays.asList(j+1,i));
+            }
+
+            if(sum == target) return new ArrayList<>(Arrays.asList(j+1,i));
+            while( j<arr.length && sum >=target){
+                sum-=arr[j++];
+                if(sum == target) return new ArrayList<>(Arrays.asList(j+1,i));
+
+            }
+            return new ArrayList<>(Arrays.asList(-1));
+        }
+    }
+}
