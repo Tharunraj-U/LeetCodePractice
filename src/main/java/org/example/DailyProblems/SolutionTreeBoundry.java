962978727b230d373c30c1c6d2bab3b46d9b6266
Tharun Raj <THARUNRAJ2023@GMAIL.COM>
2025-02-08 23:23:47 +0530

    Tupes

---
+++ src/main/java/org/example/DailyProblems/SolutionTreeBoundry.java
@@ -0,0 +1,60 @@
+package org.example.DailyProblems;
+
+import org.w3c.dom.Node;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+class SolutionTreeBoundry {
+    void addRight(ArrayList<Integer> list, Node root){
+          Stack<Integer> stack=new Stack<>();
+       if(root == null)return;
+        root=root.right;
+        while(root !=null &&  !isLeaf(root)){
+            stack.push(root.data);
+            root= root.right != null ? root.right : root.left;
+        }
+        while(!stack.isEmpty()){
+            list.add(stack.pop());
+        }
+       
+    }
+   void addLeft(Node root, ArrayList<Integer> list) {
+    if (root == null) return; // Base case
+    
+    Node curr = root; // Start from left child
+    while (curr != null && !isLeaf(curr)) {
+        list.add(curr.data); // Store the left boundary node
+        curr = (curr.left != null) ? curr.left : curr.right; // Move to left first, then right
+    }
+}
+
+    boolean isLeaf(Node root){
+       return root.right == null && root.left == null;
+    }
+     void addLeaf(Node root,ArrayList<Integer> list){
+         if(root == null)return;
+             addLeaf(root.left,list);
+             if(isLeaf(root)){
+                 list.add(root.data);
+             }
+             addLeaf(root.right,list);
+     }
+    ArrayList<Integer> boundaryTraversal(Node root) {
+       ArrayList<Integer> list=new ArrayList<>();
+       if(root == null)return list;
+       if(root.left == null && root.right == null){
+           list.add(root.data);
+           return list;
+       }
+       Node root1=root;
+       Node temp=root;
+       
+      
+       list.add(root.data);
+       addLeft(temp.left,list);
+       addLeaf(root,list);
+       addRight(list,root1);
+       return list;
+    }
+}
