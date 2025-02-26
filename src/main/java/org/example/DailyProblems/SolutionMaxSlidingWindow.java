58a10b2aefb67ace948112a3e7d4ae4bd1c55642
Tharun Raj <THARUNRAJ2023@GMAIL.COM>
2025-02-26 22:30:23 +0530

    tharun

---
+++ src/main/java/org/example/DailyProblems/SolutionMaxSlidingWindow.java
@@ -0,0 +1,23 @@
+package org.example.DailyProblems;
+
+class SolutionMaxSlidingWindow {
+    public int[] maxSlidingWindow(int[] arr, int k) {
+        int ans[]=new int[arr.length+1-k];
+        int j=0;
+        Deque<Integer> queue=new ArrayDeque<>();
+        for(int i=0;i<arr.length;i++){
+            if(!queue.isEmpty() && queue.peekFirst() <= i-k){
+                queue.removeFirst();
+            }
+            while(!queue.isEmpty()  && arr[queue.peekLast()] <=arr[i]){
+                 queue.removeLast();
+            }
+            queue.offer(i);
+            if(i >=k-1){
+                ans[j++]=(arr[queue.peekFirst()]);
+            }
+        }  
+        return ans;
+
+    }
+}
