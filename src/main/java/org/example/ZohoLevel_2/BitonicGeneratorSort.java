06097e943ef1008d445aca0132c3aa590d434686
Tharun Raj <THARUNRAJ2023@GMAIL.COM>
2025-04-24 15:43:55 +0530

    Bitonic Generator Sort

---
+++ src/main/java/org/example/ZohoLevel_2/BitonicGeneratorSort.java
@@ -0,0 +1,22 @@
+package org.example.ZohoLevel_2;
+
+class BitonicGeneratorSort
+ {
+    public static ArrayList<Integer> bitonicGenerator(int arr[]) {
+        ArrayList<Integer> list1=new ArrayList<>();
+        ArrayList<Integer> list2=new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            if((i & 1) == 0){
+              list1.add(arr[i]);  
+            }else{
+                list2.add(arr[i]);
+            }
+        }
+        Collections.sort(list1,(a,b)-> a-b);
+        Collections.sort(list2,(a,b)-> b-a);
+        for(int val:list2){
+            list1.add(val);
+        }
+        return  list1;
+    }
+}
